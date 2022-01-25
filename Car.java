466ac3334d7285858c1d0194fd422008e6586a32
adonofrio23 <96752929+adonofrio23@users.noreply.github.com>
2022-01-24 22:05:41 -0500

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,32 @@
+public class Car
+{
+	// currentSpeed is a decimal in MPH
+	float currentSpeed;
+	// currentDirection is a String. Ex: "N", "S", "SW"
+	String currentDirection;
+	// currentLocation is a decimal in degrees
+	float currentLocation;
+
+	public static void main(String[ ] args)  
+	{
+     	
+	}
+
+	// Method used to cause the car to speed up
+	void Accelerate(float toSpeed)
+	{
+
+	}
+
+	// Method used to cause the car to slow down
+	void Decelerate(float toSpeed)
+	{
+
+	}
+
+	// Method used to turn the car a certain number of degrees in a certain direction
+	void Turn(String direction, float degrees)
+	{
+
+	}
+}	
---
+++ Truck.java
@@ -0,0 +1,40 @@
+public class Truck
+{
+	// currentSpeed is a decimal in MPH
+	float currentSpeed;
+	// currentDirection is a String. Ex: "N", "S", "SW"
+	String currentDirection;
+	// currentLocation is a decimal in degrees
+	float currentLocation;
+	// loadWeight is a decimal in pounds or another applicable weight unit
+	float loadWeight;
+
+	public static void main(String[ ] args)  
+	{
+    
+	}
+
+	// Method used to cause the truck to speed up
+	void Accelerate(float toSpeed)
+	{
+
+	}
+
+	// Method used to cause the truck to slow down
+	void Decelerate(float toSpeed)
+	{
+
+	}
+
+	// Method used to turn the truck a certain number of degrees in a certain direction
+	void Turn(String direction, float degrees)
+	{
+
+	}
+
+	// Method used to set the load weight of the truck
+	void SetLoadWeight(float weight)
+	{
+		loadWeight = weight;
+	}
+}	
